f8b8f583ddca683927e9251b29ad08c3b694f5b6
vorcha100 <85402918+vorcha100@users.noreply.github.com>
2024-05-12 10:16:06 +0200

    Sunday commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        int myFirstVariable;
+        myFirstVariable = 1;
+
+        System.out.println(myFirstVariable);
+
+        String name = "Dmitry";
+        String textToPrint = "Favorite number of " + name + " is: " + myFirstVariable;
+
+        System.out.println(textToPrint);
     }
 }
